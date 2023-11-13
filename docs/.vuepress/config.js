54bc2edd02e58ca1ccf52479f4df9dc55e79d257
TheBladeHit <boqdanyarmolka@gmail.com>
2023-11-13 21:45:46 +0200

    Make config changes

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -76,7 +76,7 @@ module.exports = {
 
     lastUpdated: 'Останнє оновлення', // string | boolean
       // Assumes GitHub. Can also be a full GitLab url.
-    repo: 'https://github.com/fokaaas/db-labs',
+    repo: 'https://github.com/thebaldehit/db-labs',
     // Customising the header label
     // Defaults to "GitHub"/"GitLab"/"Bitbucket" depending on `themeConfig.repo`
     repoLabel: 'Github',
